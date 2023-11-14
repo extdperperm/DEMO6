9fbe0e9e2a92f1e795a2c14d4e0ca439c6f2eb4e
Daniel Pérez <118856521+extdperperm@users.noreply.github.com>
2023-11-14 16:20:25 +0000

    Add files via upload

---
+++ src/main/java/es/dsw/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package es.dsw.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+
+	@GetMapping(value = {"/","/index"})
+	public String idx() {
+		
+		return "index";
+	}
+	
+}
